f7cdb4156b8bffb2c63f6db1c646ec813bf3397b
Oswaldo <oswalfut_96@hotmail.com>
2021-01-15 18:41:23 -0600

    refactor inputForm component using hooks

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,56 +1,69 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { accounts, journal } from '../data';
-import * as actions from '../actions';
-import * as utils from '../utils';
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { accounts, journal } from "../data";
+import { setJournalEntries, setAccounts, setUserInput } from "../actions";
+import { parseCSV, parseUserInput } from "../utils";
 
-class InputForm extends Component {
-  state = {
-    accounts,
-    journal,
-    userInput: '1000 5000 MAR-16 JUL-16 HTML'
+function InputForm() {
+  const dispatch = useDispatch();
+  const [userInput, setUserInputText] = useState(
+    "1000 5000 MAR-16 JUL-16 HTML"
+  );
+
+  useEffect(() => {
+    dispatch(setJournalEntries(parseCSV(journal)));
+    dispatch(setAccounts(parseCSV(accounts)));
+  });
+
+  const handleChange = (e) => {
+    setUserInputText(e.target.value);
   };
 
-  componentDidMount() {
-    this.handleSubmit();
-  }
-
-  handleChange = e => {
-    this.setState({
-      [e.target.id]: e.target.value
-    });
-  }
-
-  handleSubmit = e => {
+  const handleSubmit = (e) => {
     e && e.preventDefault();
 
-    this.props.dispatch(actions.setJournalEntries(utils.parseCSV(this.state.journal)));
-    this.props.dispatch(actions.setAccounts(utils.parseCSV(this.state.accounts)));
-    this.props.dispatch(actions.setUserInput(utils.parseUserInput(this.state.userInput)));
-  }
+    dispatch(setUserInput(parseUserInput(userInput)));
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="journal">Journal</label>
+        <textarea
+          className="form-control"
+          id="journal"
+          rows="3"
+          value={journal}
+          onChange={() => {}}
+        ></textarea>
+      </div>
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div className='form-group'>
-          <label htmlFor="journal">Journal</label>
-          <textarea className="form-control" id="journal" rows="3" value={this.state.journal} onChange={this.handleChange}></textarea>
-        </div>
+      <div className="form-group">
+        <label htmlFor="accounts">Accounts</label>
+        <textarea
+          className="form-control"
+          id="accounts"
+          rows="3"
+          value={accounts}
+          onChange={() => {}}
+        ></textarea>
+      </div>
 
-        <div className='form-group'>
-          <label htmlFor="accounts">Accounts</label>
-          <textarea className="form-control" id="accounts" rows="3" value={this.state.accounts} onChange={this.handleChange}></textarea>
-        </div>
+      <div className="form-group">
+        <label htmlFor="userInput">User input</label>
+        <input
+          type="text"
+          className="form-control"
+          id="userInput"
+          value={userInput}
+          onChange={handleChange}
+        />
+      </div>
 
-        <div className="form-group">
-          <label htmlFor="userInput">User input</label>
-          <input type="text" className="form-control" id="userInput" value={this.state.userInput} onChange={this.handleChange} />
-        </div>
-
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
-    );
-  }
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
 }
 
-export default connect()(InputForm);
+export default InputForm;
